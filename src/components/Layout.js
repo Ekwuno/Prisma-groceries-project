6040e2ef973f9446c0c4212cf19d6532e72797b9
ekwuno <obinnacodes@gmail.com>
2023-02-14 17:52:37 +0000

    setup tailwind

---
+++ src/components/Layout.js
@@ -0,0 +1,17 @@
+import Head from "next/head"
+
+const Layout = () => {
+    return (
+			<>
+				<Head>
+					<title>Building new project</title>
+					<meta name="title" content="Type safe schema design with Prisma" />
+					<link rel="icon" href="/favicon.ico" />
+				</Head>
+
+				<div>
+					
+				</div>
+			</>
+		);
+}
