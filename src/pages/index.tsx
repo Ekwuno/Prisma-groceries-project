1d4debccc0f63305f2062a116ca05760906f303b
ekwuno <obinnacodes@gmail.com>
2023-02-14 17:18:41 +0000

    get client into next app

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,10 @@ import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
+import { PrismaClient } from '@prisma/client'
 
 const inter = Inter({ subsets: ['latin'] })
-
+const prisma = new PrismaClient()
 export default function Home() {
   return (
     <>
@@ -26,7 +27,7 @@ export default function Home() {
               target="_blank"
               rel="noopener noreferrer"
             >
-              By{' '}
+              {/* By{' '}
               <Image
                 src="/vercel.svg"
                 alt="Vercel Logo"
@@ -34,7 +35,7 @@ export default function Home() {
                 width={100}
                 height={24}
                 priority
-              />
+              /> */}
             </a>
           </div>
         </div>
