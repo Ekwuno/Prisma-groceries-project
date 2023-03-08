65f221477931347b776e0a277a8da7cc66630473
ekwuno <obinnacodes@gmail.com>
2023-03-08 17:45:35 +0000

    remove text

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,12 +18,6 @@ export async function getStaticProps() {
 export default function Home({grocery= []}) {
 return (
 		<Layout>
-			<h1 className="text-xl font-medium text-gray-800">
-				Top-rated places to stay
-			</h1>
-			<p className="text-gray-500">
-				Explore some of the best places in the world
-			</p>
 			<div className="mt-8">
 				<Grid grocerys={grocery} />
 			</div>
