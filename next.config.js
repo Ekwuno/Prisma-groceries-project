9b4b08c8262ee885318bc3e798cb9f571c1193f6
ekwuno <obinnacodes@gmail.com>
2023-03-08 17:46:35 +0000

    cloudinary support

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+	reactStrictMode: true,
+	images: {
+		unoptimized: true,
+		domains: ["res.cloudinary.com"],
+		loader: "cloudinary",
+		path: "res.cloudinary.com/ekwuno",
+	},
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -14,7 +14,7 @@ const Grid = ({ grocerys = [] }) => {
 	) : (
 		<div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
 			{grocerys.map(grocery => (
-				<Card key={grocery.id} {...grocery} onClickFavorite={toggleFavorite} />
+				<Card key={grocery.id} {...grocery} />
 			))}
 		</div>
 	);
